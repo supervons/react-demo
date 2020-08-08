68015fed074d913984e00df2a31d1c071f5c6041
supervons <supervons@sina.com>
2020-08-08 23:12:39 +0800

    条件渲染

--- src/view/index/HomePage.js
+++ src/view/index/HomePage.js
@@ -5,13 +5,14 @@ import React from 'react';
  * @author supervons
  * @date 2020/08/05
  */
-class HomePage extends React.Component{
+class HomePage extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state={
+        this.state = {
             text: '你好',
-            time: new Date()
+            time: new Date(),
+            condition: false
         }
         this.changeText = this.changeText.bind(this)
     }
@@ -20,15 +21,15 @@ class HomePage extends React.Component{
         this.autoTikTok()
     }
 
-    autoTikTok(){
-        setInterval(()=>{
+    autoTikTok() {
+        setInterval(() => {
             this.setState({
                 time: new Date()
             })
         }, 1000)
     }
 
-    onResize(){
+    onResize() {
         return <h1>{this.state.text}</h1>
     }
 
@@ -36,15 +37,37 @@ class HomePage extends React.Component{
         this.setState({text: '改变为点击字符'})
     }
 
+    conditionRender() {
+        if (this.state.condition) {
+            return <UserGreeting/>
+        } else {
+            return <GuestGreeting/>
+        }
+    }
+
     render() {
         return (
             <div style={{backgroundColor: 'yellow', height: '300px', width: '300px'}}>
                 {this.onResize()}
                 {this.state.time.toLocaleTimeString()}
-                <button onClick={this.changeText}>点击</button>
+                {this.conditionRender()}
+
+                <div style={{display: 'flex', flexDirection: 'column'}}>
+                    <button onClick={this.changeText}>点击</button>
+                    {/*条件渲染*/}
+                    <button onClick={()=>this.setState({condition : !this.state.condition})}>条件渲染</button>
+                </div>
             </div>
         );
     }
+}
+
+function UserGreeting(props) {
+    return <h1>Welcome back!</h1>;
+}
+
+function GuestGreeting(props) {
+    return <h1>Please sign up.</h1>;
 }
 
 export default HomePage;
